aa2508d126012d3d97334c26e61bec0d4a72d871
osunguk <osunguk@gmail.com>
2020-03-23 15:55:49 +0900

    add clear button

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -16,8 +15,10 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         val countButton: Button = findViewById(R.id.count_up)
+        val clearButton: Button = findViewById(R.id.clear_data)
         rollButton.setOnClickListener{ rollDice() }
         countButton.setOnClickListener { countUp() }
+        clearButton.setOnClickListener { clear() }
 
     }
     private fun rollDice() {
@@ -25,7 +26,17 @@ class MainActivity : AppCompatActivity() {
         val resultImage: ImageView = findViewById(R.id.dice_image)
         val randomInt = (1..6).random()
 
-        val drawbleResource = when (randomInt) {
+        val drawbleResource = pickImage(randomInt)
+
+        resultImage.setImageResource(drawbleResource)
+
+        resultText.text = randomInt.toString()
+        Toast.makeText(this, "button cliked",
+            Toast.LENGTH_SHORT).show()
+    }
+
+    fun pickImage(num: Int): Int {
+        return when (num) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -33,12 +44,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-
-        resultImage.setImageResource(drawbleResource)
-
-        resultText.text = randomInt.toString()
-        Toast.makeText(this, "button cliked",
-            Toast.LENGTH_SHORT).show()
     }
 
     private fun countUp() {
@@ -52,17 +57,18 @@ class MainActivity : AppCompatActivity() {
             if (resultText.text.toString().toInt() < 6) {
                 val num: Int = resultText.text.toString().toInt() + 1
                 resultText.text = num.toString()
-                val drawbleResource = when (num) {
-                    1 -> R.drawable.dice_1
-                    2 -> R.drawable.dice_2
-                    3 -> R.drawable.dice_3
-                    4 -> R.drawable.dice_4
-                    5 -> R.drawable.dice_5
-                    else -> R.drawable.dice_6
-                }
+                val drawbleResource = pickImage(num)
                 resultImage.setImageResource(drawbleResource)
             }
         }
+    }
+
+    private fun clear() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val resultImage: ImageView = findViewById(R.id.dice_image)
+
+        resultText.text = "Hello World!"
+        resultImage.setImageResource(R.drawable.empty_dice)
     }
 }
 
