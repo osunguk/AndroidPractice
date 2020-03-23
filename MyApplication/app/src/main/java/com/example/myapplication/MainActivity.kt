650a1445ae78f63328e1afaa8a29c24c3bef3694
osunguk <osunguk@gmail.com>
2020-03-23 15:37:07 +0900

    add dice image & add operation change image

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -21,7 +22,20 @@ class MainActivity : AppCompatActivity() {
     }
     private fun rollDice() {
         val resultText: TextView = findViewById(R.id.result_text)
+        val resultImage: ImageView = findViewById(R.id.dice_image)
         val randomInt = (1..6).random()
+
+        val drawbleResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        resultImage.setImageResource(drawbleResource)
+
         resultText.text = randomInt.toString()
         Toast.makeText(this, "button cliked",
             Toast.LENGTH_SHORT).show()
@@ -29,12 +43,24 @@ class MainActivity : AppCompatActivity() {
 
     private fun countUp() {
         val resultText: TextView = findViewById(R.id.result_text)
+        val resultImage: ImageView = findViewById(R.id.dice_image)
 
         if (resultText.text.toString() == "Hello World!"){
             resultText.text = "1"
+            resultImage.setImageResource(R.drawable.dice_1)
         } else {
             if (resultText.text.toString().toInt() < 6) {
-                resultText.text = (resultText.text.toString().toInt() + 1).toString()
+                val num: Int = resultText.text.toString().toInt() + 1
+                resultText.text = num.toString()
+                val drawbleResource = when (num) {
+                    1 -> R.drawable.dice_1
+                    2 -> R.drawable.dice_2
+                    3 -> R.drawable.dice_3
+                    4 -> R.drawable.dice_4
+                    5 -> R.drawable.dice_5
+                    else -> R.drawable.dice_6
+                }
+                resultImage.setImageResource(drawbleResource)
             }
         }
     }
